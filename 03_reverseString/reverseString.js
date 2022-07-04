ebd64c4500138b265086f80158a08e6385c37639
Eric Hand <ethehand@gmail.com>
2022-07-04 13:59:41 -0700

    Complete exercises 03 - 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+  let output = ''
 
+  for (char of input) {
+    output = char + output;
+  }
+
+  return output;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
 const removeFromArray = function() {
+let inputArray = arguments[0];
+let outputArray = [];
+let elementsToRemove = Array.prototype.slice.call(arguments,1);
+let flag = true;
+
+
+for (const input of inputArray) {
+    for (const element of elementsToRemove) {
+        if (input === element) {
+            flag = false;
+            break;
+        }
+    }
+    if (flag) { outputArray.push(input) };
+    flag = true;
+}
+return outputArray;
 
 };
+
+removeFromArray([1, 2, 3, 4], 3,4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
