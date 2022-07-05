2f1734afdaca4c0b23a8328adde74c9987028d44
Eric Hand <ethehand@gmail.com>
2022-07-04 21:36:17 -0700

    Complete exercises 05 - 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+
+    let args = Array.from(arguments);
+    let start = Math.min(...args);
+    let end = Math.max(...args);
+    let sumTotal = 0;
+
+    for (start; start <= end; start++) {
+        sumTotal += start;
+    }
+
+    return sumTotal;
 
 };
 
