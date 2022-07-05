4e6293ba534457346ab4e749b17fcfb9543510a4
Eric Hand <ethehand@gmail.com>
2022-07-04 22:39:36 -0700

    Complete exercises 07 - 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(inputTemp) {
+  let convertedTemp = (inputTemp - 32) * 5 / 9;
+  convertedTemp = Math.round((convertedTemp + Number.EPSILON) * 10) / 10;
+  return convertedTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(inputTemp) {
+  let convertedTemp = inputTemp * 9 / 5 + 32;
+  convertedTemp = Math.round((convertedTemp + Number.EPSILON) * 10) / 10;
+  return convertedTemp;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
 const add = function() {
-	
+  let args = Array.from(arguments);
+	return args.reduce((a,b) => a + b);
 };
 
 const subtract = function() {
-	
+	let args = Array.from(arguments);
+	return args.reduce((a,b) => a - b);
 };
 
 const sum = function() {
-	
+  let arg = arguments[0];
+  if (!Array.isArray(arg) || !arg.length) {
+    return 0;
+  }
+
+	return arg.reduce((a,b) => a + b);
 };
 
 const multiply = function() {
+  let arg = arguments[0];
+  if (!Array.isArray(arg) || !arg.length) {
+    return 0;
+  }
 
+	return arg.reduce((a,b) => a * b);
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
-	
+	let arg = arguments[0];
+  let total = 1;
+
+  while (arg > 0) {
+    total *= arg;
+    arg--;
+  }
+
+  return total;
+
 };
 
 // Do not edit below this line
